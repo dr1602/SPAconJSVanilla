f8003a4e87a48c602234ea6bdb94354c21583a23
dr1602 <david.arangelg@gmail.com>
2023-05-29 17:49:43 -0600

    Segundo Commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,9 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: '/dist'
     },
     resolve: {
         extensions: ['.js'],
